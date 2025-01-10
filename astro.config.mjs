e11190228e2772d0cf0ed55d713f071082ae71ae
Piyush Makwana <infopiyushmakwana@gmail.com>
2025-01-10 12:26:52 +0530

    root page is now posts page and redirecting posts/ to  /

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ export default defineConfig({
   site: "https://piyushmakwana.in/",
   base: "/",
   integrations: [sitemap(), mdx()],
+  redirects: [
+    { from: '/posts', to: '/' }
+  ],
   markdown: {
     shikiConfig: {
       theme: "material-theme-darker",
